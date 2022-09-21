6b991db0b5a0814a7595b9334f5d77c66b002565
DylanMH <dylan98909@yahoo.com>
2022-09-20 22:49:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from email.policy import default
 import re
 import kivy
 
@@ -30,20 +31,38 @@ class MainScreen(GridLayout):
         self.cols = 2
         # Length Widget
         self.add_widget(Label(text = "Length"))
-        self.length_side = FloatInput(multiline = False)
+        self.length_side = FloatInput(multiline = False, hint_text = str(0))
         self.add_widget(self.length_side)
         # Width Widget
         self.add_widget(Label(text = "Width"))
-        self.width_side = FloatInput(multiline = False)
+        self.width_side = FloatInput(multiline = False, hint_text = str(0))
         self.add_widget(self.width_side)
+        # Depth in Inches
+        self.add_widget(Label(text = 'Depth In Inches'))
+        self.depth = FloatInput(multiline = False, hint_text = str(0))
+        self.add_widget(self.depth)
         # Button Widget
         self.button = Button(text = "CALCULATION")
-        #self.button.bind(on_press = self.calculation)
+        self.button.bind(on_press = self.calculations)
         self.add_widget(self.button)
         # Area Widget
-        self.area_total = Label(text = "Area")
-        self.add_widget(self.area_total)
-        
+        self.mix_total = Label(text = "")
+        self.add_widget(self.mix_total)
+
+    def calculations(self, instance):
+        length = float(self.length_side.text)
+        width = float(self.width_side.text)
+        area = length * width
+        depth_in_feet = float(self.depth.text) / 12
+        cubic_feet = area * depth_in_feet
+        mix_total = (cubic_feet * 145) / 2000
+        self.mix_total.text = 'Total Hot Mix Needed = ' + str(mix_total)
+
+
+
+
+
+       
 class MyApp(App):
     def build(self):
         return MainScreen()
